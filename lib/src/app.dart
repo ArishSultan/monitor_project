1e5c5c3fdfe1e816fb93b0d97a43a605c2208740
arish <mkhan.mscs21seecs@seecs.edu.pk>
2021-10-31 15:31:39 +0500

    returned a basic [MaterialApp] from [build] method and initialized [AppConfig] from [App.initializeAndRun].

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:monitor_project/src/base/config.dart';
+import 'package:monitor_project/src/components/main_page.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   static Future<void> initializeAndRun() async {
+    await AppConfig.initialize();
+
     return runApp(const App._());
   }
 
   const App._({Key? key}) : super(key: key);
 
   @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: const MainPage(),
+      theme: ThemeData(fontFamily: 'Roboto'),
+    );
   }
 }
